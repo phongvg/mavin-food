22555ba480205d2a1e565de606a9e9e80ce036e0
admin admin <evaladmin@example.com>
2021-06-04 15:21:13 +0700

    Delete file /scripts/components/bepmavin.groovy

--- scripts/components/bepmavin.groovy
+++
@@ -1,7 +0,0 @@
-import org.craftercms.sites.editorial.RecipeSearchHelper
-def searchHelper = new RecipeSearchHelper(elasticsearch, urlTransformationService)
-def recipes = searchHelper.searchRecipe(null)
-println "Recipes:"
-print recipes
-
-templateModel.recipes = recipes
