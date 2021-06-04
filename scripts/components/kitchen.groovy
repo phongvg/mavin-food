7feb39ba69965f0509ded193dd08a3700507b8b5
admin admin <evaladmin@example.com>
2021-06-04 15:01:35 +0700

    Delete file /scripts/components/kitchen.groovy

--- scripts/components/kitchen.groovy
+++
@@ -1,6 +0,0 @@
-import org.craftercms.sites.editorial.NewsSearchHelper
-def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
-def recipe = searchHelper.searchNews(null)
-print recipe
-
-templateModel.recipe = recipe 
