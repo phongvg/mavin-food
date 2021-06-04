53adaeacab7be09b29f356a1c2422b0e71c4a105
admin admin <evaladmin@example.com>
2021-06-04 14:58:00 +0700

    User admin wrote content /scripts/components/kitchen.groovy

--- scripts/components/kitchen.groovy
+++ scripts/components/kitchen.groovy
@@ -1,6 +1,6 @@
-import org.craftercms.sites.editorial.RecipeSearchHelper
-def searchHelper = new RecipeSearchHelper(elasticsearch, urlTransformationService)
-def recipe = searchHelper.searchRecipe(null)
+import org.craftercms.sites.editorial.NewsSearchHelper
+def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
+def recipe = searchHelper.searchNews(null)
 print recipe
 
 templateModel.recipe = recipe 
