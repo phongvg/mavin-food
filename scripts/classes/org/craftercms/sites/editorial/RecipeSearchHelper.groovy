bcb3223147f0f57d116b932e8623f27f5d6e8134
admin admin <evaladmin@example.com>
2021-06-04 15:20:23 +0700

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/RecipeSearchHelper.groovy

--- scripts/classes/org/craftercms/sites/editorial/RecipeSearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/RecipeSearchHelper.groovy
@@ -8,7 +8,7 @@ import org.elasticsearch.search.builder.SearchSourceBuilder
 import org.elasticsearch.search.sort.FieldSortBuilder
 import org.elasticsearch.search.sort.SortOrder
 
-class NewsSearchHelper{
+class RecipeSearchHelper{
     static final String NEWS_CONTENT_TYPE ="content-type:\"/page/recipedetail\" "
     static final int DEFAULT_START = 0
     static final int DEFAULT_ROWS  = 1000
