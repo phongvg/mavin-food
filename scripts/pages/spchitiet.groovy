0d48b3437cb6cf324a0afab12514822cb4be4a28
admin admin <evaladmin@example.com>
2021-06-15 18:01:25 +0700

    User admin wrote content /scripts/pages/spchitiet.groovy

---
+++ scripts/pages/spchitiet.groovy
@@ -0,0 +1,12 @@
+import org.craftercms.sites.editorial.GroupProductSearchHelper
+import org.craftercms.sites.editorial.TaxonomySearchHelper
+
+def productGroup = contentModel.sptype_o.item.key
+
+def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
+def relatedProducts = searchHelper.searchProducts(productGroup.text,productChildGroup.text, 0)
+def categories = new TaxonomySearchHelper("nhom-sp", elasticsearch, siteItemService)
+						.getItems()
+
+templateModel.productOther = relatedProducts
+templateModel.categories = categories
