6935a21890d3034e730d16c0f02fb1e9fb1916c8
admin admin <evaladmin@example.com>
2021-06-15 18:08:47 +0700

    User admin wrote content /scripts/pages/spchitiet.groovy

--- scripts/pages/spchitiet.groovy
+++ scripts/pages/spchitiet.groovy
@@ -4,7 +4,7 @@ import org.craftercms.sites.editorial.TaxonomySearchHelper
 def productGroup = contentModel.sptype_o.item.key
 
 def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
-def relatedProducts = searchHelper.searchProducts(productGroup.text,productChildGroup.text, 0)
+def relatedProducts = searchHelper.searchProducts(productGroup.text,0)
 def categories = new TaxonomySearchHelper("nhom-sp", elasticsearch, siteItemService)
 						.getItems()
 
