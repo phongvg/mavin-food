5305c3170fc8a6bbd684e3bc443a25caace3dff1
admin admin <evaladmin@example.com>
2021-06-15 18:05:16 +0700

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/TaxonomySearchHelper.groovy

--- scripts/classes/org/craftercms/sites/editorial/TaxonomySearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/TaxonomySearchHelper.groovy
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.craftercms.blueprints.headless
+package org.craftercms.sites.editorial
 
 import groovy.util.logging.Slf4j
 
