615af172f4538a86561888b13c096a56fab3b22a
admin admin <evaladmin@example.com>
2021-06-17 14:28:56 +0700

    User admin wrote content /scripts/components/comtuyendung.groovy

--- scripts/components/comtuyendung.groovy
+++ scripts/components/comtuyendung.groovy
@@ -1,8 +1,7 @@
 import org.craftercms.sites.editorial.RecuitSearchHelper
-
 def searchHelper = new RecuitSearchHelper(elasticsearch, urlTransformationService)
 def recuits = searchHelper.searchRecuit(null)
-
+println "Recuits:"
 print recuits
 
 templateModel.recuits = recuits
