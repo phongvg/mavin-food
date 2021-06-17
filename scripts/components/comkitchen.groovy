1f606eaae638cf998c5786b60cd9edcad2481d48
admin admin <evaladmin@example.com>
2021-06-17 15:31:12 +0700

    User admin wrote content /scripts/components/comkitchen.groovy

--- scripts/components/comkitchen.groovy
+++ scripts/components/comkitchen.groovy
@@ -1,6 +1,6 @@
 import org.craftercms.sites.editorial.RecipeSearchHelper
 def searchHelper = new RecipeSearchHelper(elasticsearch, urlTransformationService)
-def recipes = searchHelper.searchRecipe(null, 0, 1)
+def recipes = searchHelper.searchRecipe(null, 0, 2)
 println "Recipes:"
 print recipes
 
