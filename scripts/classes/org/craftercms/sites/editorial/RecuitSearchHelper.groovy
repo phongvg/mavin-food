003eae8416bd460b4000bddcfef2744b25befa7e
admin admin <evaladmin@example.com>
2021-06-17 13:50:10 +0700

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/RecuitSearchHelper.groovy

---
+++ scripts/classes/org/craftercms/sites/editorial/RecuitSearchHelper.groovy
@@ -0,0 +1,77 @@
+package org.craftercms.sites.editorial
+
+import org.apache.commons.lang3.StringUtils
+import org.craftercms.engine.service.UrlTransformationService
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.FieldSortBuilder
+import org.elasticsearch.search.sort.SortOrder
+
+class RecuitSearchHelper {
+    static final String RECUIT_CONTENT_TYPE_QUERY = "content-type:\"/page/tuyendung\""
+    static final int DEFAULT_START = 0
+    static final int DEFAULT_ROWS = 1000
+    
+    def elasticsearch
+    UrlTransformationService urlTransformationService
+    
+     RecuitSearchHelper(elasticsearch, UrlTransformationService urlTransformationService) {
+        this.elasticsearch = elasticsearch
+        this.urlTransformationService = urlTransformationService
+    }
+    
+    def searchRecuit(start = DEFAULT_START, rows = DEFAULT_ROWS, additionalCriteria = null) {
+        def q = "${PRODUCT_CONTENT_TYPE_QUERY}"
+        
+        if (additionalCriteria) {
+          q = "${q} AND ${additionalCriteria}"
+        }
+        
+        def builder = new SearchSourceBuilder()
+            .query(QueryBuilders.queryStringQuery(q))
+            .from(start)
+            .size(rows)
+        
+        def result = elasticsearch.search(new SearchRequest().source(builder))
+        
+        if (result) {
+            return processRecuitListingResults(result)
+        } else {
+            result [];
+        }
+    }
+    
+    private def processRecuitListingResults(result) {
+        def recuits = []
+        
+        def documents = result.hits.hits*.getSourceAsMap()
+        
+        if (documents) {
+            documents.each {doc ->
+                def recuit = [:]
+                    recuit.title = doc.title_s
+                    recuit.date = doc.date_dt
+                    recuit.content = doc.content_html
+                    recuit.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
+                recuits << recuit
+            }
+        }
+        
+        return recuits
+    }
+    
+    private def getFieldQueryWithMultipleValues(field, values) {
+        if (values.class.isArray()) {
+          values = values as List
+        }
+    
+        if (values instanceof Iterable) {
+          values = "(" + StringUtils.join((Iterable)values, " OR ") + ")"
+        } else {
+          values = "\"${values}\""
+        }
+    
+        return "${field}:${values}"
+    }
+}
