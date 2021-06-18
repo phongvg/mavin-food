2f85c08d19673f6a7baaf06011dd0d4e19fbde08
admin admin <evaladmin@example.com>
2021-06-18 10:59:48 +0700

    User admin wrote content /scripts/components/comtailieu.groovy

--- scripts/components/comtailieu.groovy
+++ scripts/components/comtailieu.groovy
@@ -1,6 +1,5 @@
 package org.craftercms.sites.editorial.DocumentPDFSearchHelper
-
 def searchPDFHelper = new DocumentPDFSearchHelper(elasticsearch, urlTransformationService)
-def docsPDF = searchPDFHelper.searchdocs(null)
-
+def docsPDF = searchPDFHelper.searchdocs(true,0)
 templateModel.docsPDF = docsPDF
+
