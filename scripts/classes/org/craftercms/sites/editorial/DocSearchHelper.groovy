2bf55ef11a37d534ae26a477dbd2b86631a41eca
admin admin <evaladmin@example.com>
2021-06-28 11:53:02 +0700

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/DocSearchHelper.groovy

---
+++ scripts/classes/org/craftercms/sites/editorial/DocSearchHelper.groovy
@@ -0,0 +1,77 @@
+package org.craftercms.sites.editorial
+
+import org.apache.commons.lang3.StringUtils
+import org.craftercms.engine.service.UrlTransformationService
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.FieldSortBuilder
+import org.elasticsearch.search.sort.SortOrder
+
+class NewsSearchHelper{
+    static final String NEWS_CONTENT_TYPE ="content-type:\"/page/thuvientailieu\" "
+    static final int DEFAULT_START = 0
+    static final int DEFAULT_ROWS  = 1000
+    
+    def elasticsearch
+    UrlTransformationService UrlTransformationService
+    
+    NewsSearchHelper(elasticsearch, UrlTransformationService urlTransformationService) {
+        this.elasticsearch = elasticsearch
+        this.urlTransformationService = urlTransformationService
+    }
+    
+    def searchNews(categories, start = DEFAULT_START, rows = DEFAULT_ROWS, additionalCriteria = null){
+        def q = "${NEWS_CONTENT_TYPE}"
+        if(categories){
+            def categoriesQuery = getFieldQueryWithMultipleValues("categories_o.item.key",categories)
+            q = "${q} AND ${categoriesQuery}"
+        }
+        
+        if (additionalCriteria) {
+          q = "${q} AND ${additionalCriteria}"
+        }
+        
+        def builder = new SearchSourceBuilder()
+            .query(QueryBuilders.queryStringQuery(q))
+            .from(start)
+            .size(rows)
+        
+        def result = elasticsearch.search(new SearchRequest().source(builder))
+
+        if(result){
+            return processNewsListingResults(result)
+        }else{
+            return[]
+        }
+    }
+    def processNewsListingResults(result){
+        def docs= []
+        def documents = result.hits.hits*.getSourceAsMap()
+        if (documents){
+            documents.each { doc ->
+                def adoc = [:]
+                    adoc.title = doc.name_s
+                    adoc.image = doc.image_s
+                    adoc.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
+                docs << adoc
+            }
+        }
+        
+        return docs
+    }
+    
+    private def getFieldQueryWithMultipleValues(field, values) {
+        if (values.class.isArray()) {
+          values = values as List
+        }
+    
+        if (values instanceof Iterable) {
+          values = "(" + StringUtils.join((Iterable)values, " OR ") + ")"
+        } else {
+          values = "\"${values}\""
+        }
+    
+        return "${field}:${values}"
+    }
+}
