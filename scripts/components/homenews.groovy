60ccd9fcce518be4295c69654115bf4880b02155
admin admin <evaladmin@example.com>
2021-07-13 15:04:46 +0700

    User admin wrote content /scripts/components/homenews.groovy

--- scripts/components/homenews.groovy
+++ scripts/components/homenews.groovy
@@ -1,6 +1,6 @@
 import org.craftercms.sites.editorial.NewsSearchHelper
 def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
-def news = searchHelper.searchNews(0,4)
+def news = searchHelper.searchNews(4)
 println "News:"
 print news
 
